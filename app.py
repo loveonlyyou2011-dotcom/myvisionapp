e498d3e20feac63622cbadc9362807817015e846
loveonlyyou2011-dotcom <loveonlyyou2011@gmail.com>
2026-07-28 15:25:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello Streamlit-er 👋")
+st.title("안녕하세요. Choi Hyunmuk 입니다. 👋")
 st.markdown(
     """ 
     This is a playground for you to try Streamlit and have fun. 
